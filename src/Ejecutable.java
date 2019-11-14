b2d8feebcf3128c6ab9ba4941c5f6af9265cde6b
alumno <alumno@aula1-11>
2019-11-14 13:11:39 +0100

    Añadir contraseñas

--- src/Ejecutable.java
+++ src/Ejecutable.java
@@ -1,4 +1,34 @@
+import java.util.Scanner;
 
 public class Ejecutable {
-
+	private String nombre;
+	private Password[] vContraseñas;
+	
+	public Ejecutable() {
+		nombre = "";
+		vContraseñas = new Password[50];	
+	}
+	public Ejecutable(String nombre, int numPassword) {
+		this.nombre = nombre;
+		vContraseñas = new Password[numPassword];
+	}
+	public Ejecutable(String nombre) {
+		this.nombre = nombre;
+		vContraseñas = new Password[50];
+	}
+	
+	public void añadirContraseñas() {
+		Scanner leer = new Scanner(System.in);
+		int tam;
+		
+		System.out.println("Dime el tamaño de la contraseña");
+		tam = leer.nextInt();
+		
+		for (int i = 0; i < vContraseñas.length; i++) {
+			if (vContraseñas[i]==null) {
+				vContraseñas[i] = new Password(tam);
+				break;
+			}
+		}
+	}
 }
