10b2572a1be5945a35ae2c6656a1cba8bf33a5ce
alumno <alumno@aula1-11>
2019-11-14 13:01:09 +0100

    vectores acabado

--- src/Principal.java
+++ src/Principal.java
@@ -13,8 +13,9 @@ public class Principal {
 		boolean [] vFuerteDebiil = new boolean[tamañoVector];
 		
 		for (int i = 0; i < tamañoVector; i++) {
-			Password pas = new Password(tamañoVector);
-			
+			vPasswords[i] = new Password(tamañoVector);
+			vFuerteDebiil[i] = vPasswords[i].esFuerte();
+			System.out.println(vPasswords[i].getContraseña() + " - " + vFuerteDebiil[i]);
 		}
 		
 	}
